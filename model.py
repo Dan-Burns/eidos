97886802b68150d31a69ec36320eeb42d18cc76a
Dan-Burns <dnlburns808@gmail.com>
2023-11-09 09:24:29 -0600

    utils and parmed underscore

--- model.py
+++ model.py
@@ -51,3 +51,23 @@ class MolFromPDB():
         if output.split('.')[-1] is not 'mol':
             output = f'{output}.mol'
         Chem.MolToMolFile(self.mol,output)
+
+def parmed_underscore_topology(gromacs_processed_top, atom_indices, output_top):
+    '''
+    Add underscores to atom types of selected atoms.
+    This is useful if using the plumed_scaled_topologies script 
+    for hremd system modification.
+    With this, you still need to open the new topology file and delete the 
+    underscores from the beginning of the file [atomtypes]
+    or else plumed will look for atoms with 2 underscores to apply lambda to.
+    '''
+    top = GromacsTopologyFile(gromacs_processed_top)
+
+    for atom in top.view[atom_indices].atoms:
+        atom.type = f"{atom.type}_"
+        if atom.atom_type is not pmd.UnassignedAtomType:
+            atom.atom_type = copy.deepcopy(atom.atom_type)
+            atom.atom_type.name = f"{atom.atom_type.name}_"
+
+
+    top.save(output_top)
